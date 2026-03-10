9276b806b62fa9d3dfa21f13ce1dc75d85a56734
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-03-10 13:33:43 +0600

    jobdetails

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,6 +3,8 @@ import MainLayouts from "../Layouts/MainLayouts";
 import Home from "../Pages/Home";
 import Register from './../Pages/Register/Register';
 import Login from './../Pages/Login/Login';
+import JobDetails from "../components/JobRelated/JobDetails";
+import axios from "axios";
 
 
 const router = createBrowserRouter([
@@ -22,10 +24,11 @@ const router = createBrowserRouter([
                 path:"/register",
                 Component:Register
             },
-            // {
-            //     path:"add-job",
-            //     Component:Home
-            // },
+            {
+                path:"jobs/:jobId",
+                Component:JobDetails,
+                loader:({params})=>axios(`${import.meta.env.VITE_API_KEY}/jobs/${params.jobId}`)
+            },
             // {
             //     path:true,
             //     Component:Home
